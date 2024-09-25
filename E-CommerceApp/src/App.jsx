769d0be470d5db25360d342403b89c5050b8c329
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-09-25 19:31:19 +0530

    SetUp new project E-commerceApp

---
+++ E-CommerceApp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './customer/components/navigation/Navigation'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navigation />
+      </div>
+    </div>
+  )
+}
+
+export default App
