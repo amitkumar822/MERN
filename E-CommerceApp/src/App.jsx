f31ade70c072584b111a1888b5c389369400831a
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-09-28 22:40:10 +0530

    remove file import

--- E-CommerceApp/src/App.jsx
+++ E-CommerceApp/src/App.jsx
@@ -1,13 +1,4 @@
 import React from "react";
-import Navigation from "./customer/components/navigation/Navigation";
-import HomePage from "./customer/pages/HomePage/HomePage";
-import Footer from "./customer/components/Footer/Footer";
-import Product from "./customer/components/Product/Product";
-import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
-import Cart from "./customer/components/Cart/Cart";
-import CheckOut from "./customer/components/CheckOut/CheckOut";
-import Order from "./customer/components/Order/Order";
-import OrderDetails from "./customer/components/Order/OrderDetails";
 import { Route, Routes } from "react-router-dom";
 import CustomerRouters from "./Routers/CustomerRouters";
 
