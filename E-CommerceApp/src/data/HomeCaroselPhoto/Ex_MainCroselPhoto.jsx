c01fb1df8cdfdcce27e01084406c663be301f4c1
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-09-29 09:00:34 +0530

    Add some Banner Image

---
+++ E-CommerceApp/src/data/HomeCaroselPhoto/Ex_MainCroselPhoto.jsx
@@ -0,0 +1,6 @@
+import Banner1 from "./Banner1.png";
+import Banner2 from "./Banner2.png";
+import Banner3 from "./Banner3.png";
+import Banner4 from "./Banner4.png";
+
+export const Ex_MainCroselPhot = { Banner1, Banner2, Banner3, Banner4 };
