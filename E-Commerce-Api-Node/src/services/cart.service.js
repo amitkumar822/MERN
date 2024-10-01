c1d4b1a01b6b87c79640d7ec81f54e6ace812981
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-01 14:52:42 +0530

    service card add

--- E-Commerce-Api-Node/src/services/cart.service.js
+++ E-Commerce-Api-Node/src/services/cart.service.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cart.model");
+const CartItem = require("../models/cartItem.model");
 
 const createCart = async (user) => {
   try {
@@ -10,4 +11,65 @@ const createCart = async (user) => {
   }
 };
 
-module.exports = { createCart };
+const findUserCart = async (userId) => {
+  try {
+    let cart = await Cart.findOne({ user: user });
+
+    let cartItems = await CartItem.find({ cart: cart._id }).populate("product");
+
+    cart.cartItems = cartItems;
+
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+
+    for (let cartItem of cart.cartItems) {
+      totalPrice += cartItem.price;
+      totalDiscountedPrice += cartItem.discountedPrice;
+      totalItem += cartItem.quantity;
+    }
+
+    cart.totalPrice = totalPrice;
+    cart.totalItem = totalItem;
+    cart.discounte = totalPrice - totalDiscountedPrice;
+
+    return cart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const addCartItem = async (userId, req) => {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+    const product = await Product.findById(req.productId);
+
+    const isPresent = await CartItem.findOne({
+      cart: cart._id,
+      product: product._id,
+      userId,
+    });
+
+    if (!isPresent) {
+      const cartItem = new CartItem({
+        product: product._id,
+        cart: cart._id,
+        quantity: 1,
+        userId,
+        price: product.price,
+        size: req.size,
+        discountedPrice: product.discountedPrice,
+      });
+
+      const createdCartItem = await CartItem.save();
+      cart.cartItems.push(createdCartItem);
+      await cart.save();
+
+      return "Item added to cart";
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = { createCart, findUserCart, addCartItem };
---
+++ E-Commerce-Api-Node/src/services/orderService.js
@@ -0,0 +1,135 @@
+const Address = require("../models/address.model.js");
+const Order = require("../models/order.model.js");
+const cartService = require("../services/cart.service.js");
+
+const createOrder = async (user, shippAddress) => {
+  let address;
+
+  if (shippAddress._id) {
+    let existAddress = await Address.findById(shippAddress._id);
+    address = existAddress;
+  } else {
+    address = new Address(shippAddress);
+    address.user = user;
+    await address.save();
+
+    user.addresses.push(address);
+    await user.save();
+  }
+
+  const cart = await cartService.findUserCart(user.id);
+  const orderItems = [];
+
+  for (let item of cart.cartItems) {
+    const orderItem = new orderItem({
+      price: item.price,
+      product: item.product,
+      quantity: item.quantity,
+      size: item.size,
+      userId: item.userId,
+      discountedPrice: item.discountedPrice,
+    });
+
+    const createdOrderItem = await orderItem.save();
+    orderItems.push(createdOrderItem);
+  }
+
+  const createdOrder = new Order({
+    user,
+    orderItems,
+    totalPrice: cart.totalPrice,
+    totalDiscountedPrice: cart.totalDiscountedPrice,
+    discounte: cart.discounted,
+    totalItem: cart.totalItem,
+    shippAddress: address,
+  });
+
+  const savedOrder = await createOrder.save();
+  return savedOrder;
+};
+
+const placeOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "PLACED";
+  order.paymentDetails.status = "COMPLETED";
+
+  return await order.save();
+};
+
+const confirmedOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "CONFIRMED";
+
+  return await order.save();
+};
+
+const shipOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "SHIPPED";
+
+  return await order.save();
+};
+
+const deliverOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "DELIVERED";
+
+  return await order.save();
+};
+
+const canclledOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "CANCELLED";
+
+  return await order.save();
+};
+
+const findOrderById = async (orderId) => {
+  const order = await Order.findById(orderId)
+    .populate("user")
+    .populate({ path: "orderItems", populate: { path: "product" } })
+    .populate("shippingAddress");
+
+  return order;
+};
+
+const usersOrderHistory = async (userId) => {
+  try {
+    const orders = await Order.find({ user: userId, orderStatus: "PLACED" })
+      .populate({ path: "orderItems", populate: { path: "product" } })
+      .lean();
+
+    return orders;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getAllOrders = async () => {
+  return await Order.find()
+    .populate({ path: "orderItems", populate: { path: "product" } })
+    .lean();
+};
+
+const deleteOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+  await Order.findByIdAndDelete(order._id);
+};
+
+module.exports = {
+  createOrder,
+  placeOrder,
+  confirmedOrder,
+  shipOrder,
+  deliverOrder,
+  canclledOrder,
+  findOrderById,
+  usersOrderHistory,
+  getAllOrders,
+  deleteOrder,
+};
