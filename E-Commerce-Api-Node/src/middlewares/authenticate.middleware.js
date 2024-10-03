b2cb4ea5cfa9306277c11d1b966f4b1a8322efb7
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-03 13:12:15 +0530

    fix typo header to replace headers and quthorization to authorization fix typo error

--- E-Commerce-Api-Node/src/middlewares/authenticate.middleware.js
+++ E-Commerce-Api-Node/src/middlewares/authenticate.middleware.js
@@ -2,15 +2,19 @@ const jwtProvider = require("../config/jwtProvider.js");
 const userService = require("../services/user.service.js");
 
 const authenticate = async (req, res, next) => {
-  // Bearer token....
   try {
-    const token = req.header.quthorization?.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
+
     if (!token) {
       return res.status(404).send({ error: "token not found" });
     }
 
     const userId = jwtProvider.getUserIdFromToken(token);
-    const user = userService.findUserById(userId);
+    const user = await userService.findUserById(userId);
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
     req.user = user;
   } catch (error) {
     return res.status(500).send({ error: error.message });
