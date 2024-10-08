c5cfd943c2eded62ef963704cd54f7083733ad92
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-08 16:24:05 +0530

    add some more components

--- E-CommerceApp/src/State/Store.js
+++ E-CommerceApp/src/State/Store.js
@@ -1,10 +1,14 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import { thunk } from "redux-thunk"; 
+import { thunk } from "redux-thunk";
 
 import { authReducer } from "./Auth/Reducer";
+import { customerProductReducer } from "./Product/Reducer";
+import { cartReducer } from "./Cart/Reducer";
 
 const rootReducers = combineReducers({
   auth: authReducer,
+  product: customerProductReducer,
+  cart: cartReducer,
 });
 
 export const Store = legacy_createStore(rootReducers, applyMiddleware(thunk));
