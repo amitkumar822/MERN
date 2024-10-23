6fa23f691612b69f7bf1533e3ffcbfda65db05b2
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-23 12:18:01 +0530

    add all pages

--- BlogApp/FrontEnd/src/App.jsx
+++ BlogApp/FrontEnd/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import { Route, Routes, useLocation } from "react-router-dom"
+import Blogs from './pages/Blogs'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 function App() {
+  const location = useLocation();
+  const hideNavbarFooter = ["/dashboard","/login","/register"].includes(location.pathname);
   return (
     <div>
-      <h1 className='text-3xl text-center bg-orange-400 py-3'>Hello India!</h1>
+      {!hideNavbarFooter && <Navbar />}
+
+      {/* Difining router */}
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      {!hideNavbarFooter && <Footer />}
     </div>
   )
 }
