c66355c410759135d0a1f5376de39691b2ca4ef7
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-23 15:37:30 +0530

    use crose middleware

--- BlogApp/Backend/src/app.js
+++ BlogApp/Backend/src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express();
 
 // middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(
   fileUpload({
