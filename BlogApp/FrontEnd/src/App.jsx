b4cdf92da6ecd6390653b15b3fe38bf01aee018a
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-23 11:08:19 +0530

    setup frontend

---
+++ BlogApp/FrontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-center bg-orange-400 py-3'>Hello India!</h1>
+    </div>
+  )
+}
+
+export default App
