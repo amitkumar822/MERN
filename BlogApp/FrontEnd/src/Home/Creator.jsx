08eee8c31cdb3cf2805641449f912210948916e0
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-24 14:02:55 +0530

    creater component completed

--- BlogApp/FrontEnd/src/Home/Creator.jsx
+++ BlogApp/FrontEnd/src/Home/Creator.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Creator = () => {
-  return (
-    <div>Creator</div>
-  )
-}
+  const [admin, setAdmin] = useState();
 
-export default Creator
+  useEffect(() => {
+    const fetchAdmin = async () => {
+      try {
+        const { data } = await axios.get("/api/users/get-admin");
+        console.log("Response: ", data.admin);
+        setAdmin(data.admin);
+      } catch (error) {
+        console.log("Error fetching admin: ", error);
+      }
+    };
+    fetchAdmin();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-semibold mb-6">Popular Creators</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5">
+        {admin && admin.length > 0 ? (
+          admin.slice(0, 6).map((element, index) => {
+            return (
+              <div
+                key={index}
+              >
+                <Link to={`/`}>
+                  <div className="flex flex-col items-center -ml-14">
+                    <img
+                      src={element?.photo?.url}
+                      className="w-52 h-52 object-cover border border-black rounded-full items-center"
+                      alt="creater"
+                    />
+                    <div className="text-center">
+                      <p>{element?.name}</p>
+                      <p className="text-gray-600 text-xs">{element?.role}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Creator;
