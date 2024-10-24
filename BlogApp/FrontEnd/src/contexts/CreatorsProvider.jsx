d1eace4740d48d6c03d5c334417d522f7c695ba2
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-24 16:37:13 +0530

    setup creatorsProvider pags

---
+++ BlogApp/FrontEnd/src/contexts/CreatorsProvider.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+export const CreatorsContext = createContext();
+
+export const CreatorsProvider = ({ children }) => {
+  const [creators, setCreators] = useState();
+
+  useEffect(() => {
+    const fetchCreators = async () => {
+      try {
+        const { data } = await axios.get("/api/users/get-admin");
+        // console.log(data);
+        setCreators(data.admin);
+      } catch (error) {
+        console.log("Error CreatorsProvider", error);
+      }
+    };
+    fetchCreators();
+  }, [setCreators]);
+  return (
+    <CreatorsContext.Provider value={{ creators }}>
+      {children}
+    </CreatorsContext.Provider>
+  );
+};
+
+export const useCreators = () =>  useContext(CreatorsContext);
