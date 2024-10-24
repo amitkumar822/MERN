bfa1e5d92382fee1bfcfb2374306fdcb02bbd605
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-24 13:29:40 +0530

    Tranding complete

--- BlogApp/FrontEnd/src/Home/Trending.jsx
+++ BlogApp/FrontEnd/src/Home/Trending.jsx
@@ -28,41 +28,50 @@ const Trending = () => {
   };
 
   return (
-    <div>
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-semibold mb-4">Trending</h1>
       <Carousel responsive={responsive}>
         {blogs && blogs.length > 0 ? (
-          blogs.map((element) => {
+          blogs.slice(0, 6).map((element, index) => {
             return (
-              <Link
-                to={`/`}
-                key={element._id}
-                className="bg-white rounded-lg hover:shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300"
+              <div
+                key={index}
+                className="m-2 px-4 py-4 rounded-lg border border-gray-400 overflow-hidden"
               >
-                <div className="group relative">
-                  <img
-                    src={element?.blogImage?.url}
-                    className="w-full h-56 object-cover"
-                    alt=""
-                  />
-                  <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-75 group-hover:opacity-100 transition-transform duration-300"></div>
-                  <h1 className="absolute bottom-4 left-4 text-white bg-[rgba(0,0,0,0.3)] px-2 -ml-4 rounded-r-md font-bold text-xl group-hover:text-yellow-400 transition-colors duration-300">
-                    {element?.title}
-                  </h1>
-                </div>
-                <div className="flex p-6 items-center">
-                  <img
-                    src={element?.adminPhoto.url}
-                    className="w-12 h-12 object-cover rounded-full border-2 border-yellow-400"
-                    alt=""
-                  />
-                  <div className="ml-4">
-                    <p className="text-lg font-semibold text-gray-800">
-                      {element.adminName}
-                    </p>
-                    <p className="text-xs text-gray-400">New</p>
+                <Link to={`/`}>
+                  <div className=" relative">
+                    <div className="bg-orange-500 w-54 h-48 rounded-t-lg overflow-hidden">
+                      <img
+                        src={element?.blogImage?.url}
+                        className="w-full h-full"
+                        alt="blog"
+                      />
+                    </div>
+                    <div className="absolute top-4 left-4 bg-blue-500 text-white px-3 py-1 rounded-full text-sm">
+                      {element.category}
+                    </div>
+                    <h1
+                      className="font-bold mb-2 mt-4 overflow-hidden text-ellipsis"
+                      style={{ whiteSpace: "nowrap" }}
+                    >
+                      {element?.title}
+                    </h1>
                   </div>
-                </div>
-              </Link>
+                  <div className="flex items-center mt-4">
+                    <img
+                      src={element?.adminPhoto.url}
+                      className="w-8 h-8 object-cover rounded-full border-2 border-yellow-400"
+                      alt="author_avatar"
+                    />
+                    <div className="ml-4">
+                      <p className="text-sm font-semibold text-gray-500">
+                        {element.adminName}
+                      </p>
+                      {/* <p className="text-xs text-gray-400">New</p> */}
+                    </div>
+                  </div>
+                </Link>
+              </div>
             );
           })
         ) : (
