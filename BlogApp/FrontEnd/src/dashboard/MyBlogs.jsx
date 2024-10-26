445e69edb2fff221b0644f8d5b60fb7b52a7221e
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-26 21:00:31 +0530

    fix bug

--- BlogApp/FrontEnd/src/dashboard/MyBlogs.jsx
+++ BlogApp/FrontEnd/src/dashboard/MyBlogs.jsx
@@ -4,24 +4,30 @@ import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 
 const MyBlogs = () => {
-  // const [myBlogs, setMyBlogs] = useState(
-  //   JSON.parse(localStorage.getItem("myBlogs"))
-  // );
   const [myBlogs, setMyBlogs] = useState();
+  const [blogId, setBlogId] = useState("");
 
   useEffect(() => {
     const fetchMyBlogs = async () => {
       try {
         const { data } = await axios.get("/api/blogs/get-my-blog");
-        // localStorage.setItem("myBlogs", JSON.stringify(data.blogs));
         setMyBlogs(data.blog);
-        // toast.success("My Blogs fetched successfully");
       } catch (error) {
         console.error(error.message);
       }
     };
     fetchMyBlogs();
   }, []);
+
+  const handleDeleteProfile = async () => {
+    try {
+      await axios.delete(`/api/blogs/delete/${blogId}`);
+      toast.success("Blog deleted successfully");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message || "Faild to delete blog")
+    }
+  }
 
   return (
     <div>
@@ -54,7 +60,10 @@ const MyBlogs = () => {
                     >
                       UPDATE
                     </Link>
-                    <button className="text-red-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline">
+                    <button
+                      onClick={() => setBlogId(element?._id)}
+                      className="text-red-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline"
+                    >
                       DELETE
                     </button>
                   </div>
@@ -66,6 +75,33 @@ const MyBlogs = () => {
               You have not posted any blog to see!
             </p>
           )}
+
+          {/* Delete Confirmation Modal */}
+          <div>
+            {blogId && (
+              <div className="absolute inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
+                <div className="bg-white rounded-lg p-6 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg shadow-lg">
+                  <h1 className="text-lg md:text-2xl text-gray-800 font-semibold text-center mb-4">
+                    Are you sure you want to delete the profile?
+                  </h1>
+                  <div className="flex justify-around mt-4">
+                    <button
+                      onClick={() => setBlogId("")}
+                      className="bg-green-600 hover:bg-green-700 duration-300 text-white font-semibold px-4 py-2 rounded-lg"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={handleDeleteProfile}
+                      className="bg-red-600 hover:bg-red-700 duration-300 text-white font-semibold px-4 py-2 rounded-lg"
+                    >
+                      Yes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
