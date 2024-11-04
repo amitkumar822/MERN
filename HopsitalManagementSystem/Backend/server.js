a2334344bec88fabfadd89baaf1b677204ca0295
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-04 23:26:30 +0530

    connect database

--- HopsitalManagementSystem/Backend/server.js
+++ HopsitalManagementSystem/Backend/server.js
@@ -1,10 +1,18 @@
 import app from "./app.js";
+import { connectDB } from "./db/index.js";
 
 app.get("/", (req, res) => {
   res.send("Welcome to the express server!");
 });
 
 const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed: " + error);
+  });
