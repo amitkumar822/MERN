67f0f327fd6db1602108712923235c796af9cb2d
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-04 23:26:10 +0530

    set midleware

--- HopsitalManagementSystem/Backend/app.js
+++ HopsitalManagementSystem/Backend/app.js
@@ -1,9 +1,28 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 config({ path: "./config/config.env" });
 
 // middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URI || process.env.DASHBOARD_URI],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 export default app;
