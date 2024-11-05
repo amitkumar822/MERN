7b98d131552ff5e18885c1d88917b9272a9f432f
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-05 22:51:29 +0530

    fix some bugs and remove Unnecessary line.

--- HopsitalManagementSystem/Backend/controllers/message.controller.js
+++ HopsitalManagementSystem/Backend/controllers/message.controller.js
@@ -1,11 +1,11 @@
 import { Message } from "../models/message.model.js";
 import ApiErrorHandler from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+
 export const sendMessage = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
 
   if (!firstName || !lastName || !email || !phone || !message) {
-    // return res.status(400).json({ message: "All fields are required" });
     return next(new ApiErrorHandler("All fields are required", 400));
   }
 
@@ -18,11 +18,4 @@ export const sendMessage = asyncHandler(async (req, res, next) => {
   });
   await newMessage.save();
   res.status(201).json({ message: "Message sent successfully" });
-
-  // try {
-
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ message: "Server error, please try again later" });
-  // }
 });
