b79b64d12e9fc5503ef2f9eb55a84bb4330ddf0a
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-07 14:20:39 +0530

    setup fronted hospital management system

---
+++ HopsitalManagementSystem/FrontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center text-3xl bg-orange-500 py-2'>App</h1>
+    </div>
+  )
+}
+
+export default App
