01d548d51eaec8ec4f5ab75408c93f0871fc05e5
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-08 11:58:27 +0530

    add tostify

--- EcommerceApp2/frontend/src/App.jsx
+++ EcommerceApp2/frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Outlet />
       </main>
       <Footer />
+
+      <ToastContainer position={"top-center"} />
     </div>
   );
 }
