7d41d661cba34aaf2f4426c832357fec53e927ce
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-08 16:54:07 +0530

    add role in user model

--- EcommerceApp2/backend/src/models/user.model.js
+++ EcommerceApp2/backend/src/models/user.model.js
@@ -27,6 +27,11 @@ const userSchema = new Schema(
     },
     token: {
       type: String
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["ADMIN", "GENERAL"],
     }
   },
   { timestamps: true }
