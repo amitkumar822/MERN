947c46e14f5786cf0c0422066476acc598d218e4
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-08 16:53:51 +0530

    complete user authenticated

---
+++ EcommerceApp2/backend/src/middlewares/userAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import { ApiError } from "../../utils/ApiError.js";
+
+export const isAuthenticated = asyncHandler(async (req, res, next) => {
+  const token = req.cookies?.jwt;
+
+  if (!token) {
+    throw new ApiError(401, "User Not Authenticated");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  const user = await User.findById(decodedToken.userId);
+
+  if (!user) {
+    throw new ApiError(404, "User Not Found");
+  }
+
+  req.user = user;
+  next();
+});
