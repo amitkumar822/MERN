a0abba6507a79e076009fc33e16888a141fd1932
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 10:12:35 +0530

    create context and context provider

---
+++ EcommerceApp2/frontend/src/context/UserContextProvider.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import UserContext from "./userContext";
+import axios from "axios";
+
+const UserContextProvider = ({ children }) => {
+  const [userDetails, setUserDetails] = useState({});
+
+  const fetchUserDetails = async () => {
+    try {
+      const { data } = await axios.get("/api/user/get-user-details", {
+        credentials: "include",
+      });
+      setUserDetails(data);
+    } catch (error) {
+      console.log(
+        "Error Featching User Details ",
+        error.response.data.message || "Intrnal Server Error"
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{ userDetails, setUserDetails, fetchUserDetails }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
