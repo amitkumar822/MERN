b7eaaff7e6be8b1653fa61b3951131c810978246
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 12:51:56 +0530

    add cloudinary for upload avatar

---
+++ EcommerceApp2/backend/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { config } from "dotenv";
+config({ path: ".env" });
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    console.log("LoaclFilePath: " + localFilePath);
+  try {
+    if (!localFilePath) return null;
+    // upload the file on the cloudinary server
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log("Error uploading file: ", error);
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
