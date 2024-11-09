104d535b554e385cdcb0f898da557db4365ee062
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 12:52:15 +0530

    multer midleware set in registration file

--- EcommerceApp2/backend/src/routes/user.routes.js
+++ EcommerceApp2/backend/src/routes/user.routes.js
@@ -6,10 +6,21 @@ import {
   logOut,
 } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middlewares/userAuth.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.post(
+  "/register",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+      // limits: { fileSize: 10 * 1024 * 1024 },
+    },
+  ]),
+  registerUser
+);
 router.post("/login", loginUser);
 router.post("/logout", isAuthenticated, logOut);
 router.get("/get-user-details", isAuthenticated, getUserDetails);
