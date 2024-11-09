155c8f8195ef1ccafd879c09633cd940f2f58d1f
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 11:21:55 +0530

    Wrap React Redux Provider

--- EcommerceApp2/frontend/src/main.jsx
+++ EcommerceApp2/frontend/src/main.jsx
@@ -4,9 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router.jsx";
 import UserContextProvider from "./context/UserContextProvider.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <UserContextProvider>
-    <RouterProvider router={router} />
-  </UserContextProvider>
+  <Provider store={store}>
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  </Provider>
 );
