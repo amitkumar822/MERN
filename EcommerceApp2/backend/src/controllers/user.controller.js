15ef100d29b60b323aba8923414073493ec91aa4
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 12:54:07 +0530

    set regster controller upload avatar

--- EcommerceApp2/backend/src/controllers/user.controller.js
+++ EcommerceApp2/backend/src/controllers/user.controller.js
@@ -1,11 +1,27 @@
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../../utils/cloudinary.js";
 import createTokenAndSaveCookie from "../jwt/AuthToken.js";
 import { User } from "../models/user.model.js";
 
 // Register User Endpoint
 export const registerUser = asyncHandler(async (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    throw new ApiError(400, "User photo must be required!");
+  }
+
+  const avatar = req.files?.avatar[0];
+
+  const allowedFormats = ["image/jpeg", "image/png", "image/webp"];
+
+  if (!allowedFormats.includes(avatar.mimetype)) {
+    throw new ApiError(
+      400,
+      "Invalid avatar format, Only jpeg, png and webp are allowed!"
+    );
+  }
+
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -16,9 +32,25 @@ export const registerUser = asyncHandler(async (req, res) => {
   if (user) {
     throw new ApiError(400, "User All Ready Register With This Email!");
   }
+  // console.log("Avatar", avatar)
+  const cloudinarResponse = await uploadOnCloudinary(avatar?.path);
+  if (cloudinarResponse === null) {
+    throw new ApiError(400, "Failed to upload avatar!");
+  }
 
-  const newUser = new User({ name, email, password, role: "GENERAL" });
-  const savedUser = await newUser.save(); // password remove in response
+  const newUser = new User({
+    name,
+    email,
+    password,
+    role: "GENERAL",
+    avatar: {
+      public_id: cloudinarResponse.public_id,
+      url: cloudinarResponse.url,
+    },
+  });
+
+  const savedUser = await newUser.save();
+  // password remove in response
   savedUser.password = "";
 
   // generate token and save
