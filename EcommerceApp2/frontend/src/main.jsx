c501aaf6e667e7e6b7215a82bf2e0149f2a7b3df
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-09 10:13:20 +0530

    add UserContextProvider

--- EcommerceApp2/frontend/src/main.jsx
+++ EcommerceApp2/frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router.jsx";
+import UserContextProvider from "./context/UserContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <UserContextProvider>
     <RouterProvider router={router} />
-  </StrictMode>
+  </UserContextProvider>
 );
