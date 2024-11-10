ee079ffc5cf9de703f3de8c4569024ae992f4ccc
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-11 00:22:31 +0530

    add destroy image on cloudinary utils

---
+++ EcommerceApp2/backend/utils/deleteFromCloudinary.js
@@ -0,0 +1,18 @@
+import cloudinary from "cloudinary";
+
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Function to delete an image from Cloudinary
+export const deleteFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.error(`Failed to delete image with public_id ${publicId}: `, error);
+    throw new Error(`Failed to delete image with public_id ${publicId}`);
+  }
+};
