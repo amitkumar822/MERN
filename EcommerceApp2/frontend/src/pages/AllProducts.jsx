1076a213147da20bef6290f546f6e6fef40fcb8f
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-10 19:47:11 +0530

    add new componets

--- EcommerceApp2/frontend/src/pages/AllProducts.jsx
+++ EcommerceApp2/frontend/src/pages/AllProducts.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AllProducts = () => {
   return (
-    <div>AllProducts</div>
-  )
-}
+    <div className="w-full max-h-[calc(100vh-120px)] overflow-y-auto md:flex hidden pt-1">
+      <div className="w-full py-2 px-4 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Product</h2>
+        <button className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full ">
+          Upload Product
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default AllProducts
+export default AllProducts;
