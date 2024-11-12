22ddec75dce8a28cad31b3c078720165a8ec7636
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-12 12:36:38 +0530

    comment line remove

--- EcommerceApp2/frontend/src/components/DisplayImage.jsx
+++ EcommerceApp2/frontend/src/components/DisplayImage.jsx
@@ -4,7 +4,6 @@ const DisplayImage = ({ imgUrl }) => {
   return (
     <dialog id="my_modal_4" className="modal">
       <div className="modal-box w-fit h-fit">
-        {/* <h3 className="font-bold text-lg"></h3> */}
         <div className="max-w-[27rem] max-h-[27rem]">
           <img src={imgUrl} alt="Product Image" />
         </div>
