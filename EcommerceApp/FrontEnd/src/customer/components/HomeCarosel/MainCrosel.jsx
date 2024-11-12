9d9f2ff1f87254cebffc0be41fd478b73b3d5c3e
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-12 23:01:06 +0530

    format

--- EcommerceApp/FrontEnd/src/customer/components/HomeCarosel/MainCrosel.jsx
+++ EcommerceApp/FrontEnd/src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import { Ex_MainCroselPhot } from '../../../data/HomeCaroselPhoto/Ex_MainCroselPhoto';
-
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Ex_MainCroselPhot } from "../../../data/HomeCaroselPhoto/Ex_MainCroselPhoto";
 
 const items = [
-    <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
-        <img src={Ex_MainCroselPhot.Banner1} alt="" />
-    </div>,
-    <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
-        <img src={Ex_MainCroselPhot.Banner2} alt="" />
-    </div>,
-    <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
-        <img src={Ex_MainCroselPhot.Banner3} alt="" />
-    </div>,
-    <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
-        <img src={Ex_MainCroselPhot.Banner4} alt="" />
-    </div>,
+  <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
+    <img src={Ex_MainCroselPhot.Banner1} alt="" />
+  </div>,
+  <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
+    <img src={Ex_MainCroselPhot.Banner2} alt="" />
+  </div>,
+  <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
+    <img src={Ex_MainCroselPhot.Banner3} alt="" />
+  </div>,
+  <div className="item h-[520px] overflow-hidden bg-green-300" data-value="1">
+    <img src={Ex_MainCroselPhot.Banner4} alt="" />
+  </div>,
 ];
 
 export const MainCrosel = () => (
-    <div className='mt-2'>
-        <AliceCarousel
-        items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1500}
-        infinite
+  <div className="mt-2">
+    <AliceCarousel
+      items={items}
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={1500}
+      infinite
     />
-    </div>
+  </div>
 );
