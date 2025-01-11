1ca8f499b764ed58a16b6d95d024725b80233db0
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-01-11 12:12:34 +0530

    Add initial backend and frontend setup for LMS application
    - Created package.json and package-lock.json for both backend and frontend, defining dependencies for Express, Mongoose, React, and Tailwind CSS.
    - Established basic project structure with essential files including index.html, main.jsx, and App.jsx for the frontend.
    - Configured ESLint and PostCSS for code quality and styling.
    - Added Tailwind CSS configuration for styling and responsive design.
    - Implemented a button component with variant support for UI consistency.
    
    These changes lay the foundation for the LMS application, enabling further development and feature integration.

---
+++ LMS/FrontEnd/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-center bg-orange-400 py-2'>Hello</h1>
+      <Button>SadCN</Button>
+    </div>
+  )
+}
+
+export default App
