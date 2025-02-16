fde6a5d41e74022a4e312bee7984ceae8f27bf7b
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-02-16 10:39:40 +0530

    Minor update to Vite proxy configuration comment

--- AmiShop/frontend/vite.config.js
+++ AmiShop/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:4000/api/v1', // URL of your backend
-        target: 'https://amishop-api.vercel.app/api/v1', // URL of your backend
+        target: 'https://amishop-api.vercel.app/api/v1', // Vercel URL of your backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
