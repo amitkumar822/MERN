167d45742ca81b882e2c4a7cd204138f366e6459
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-02-20 17:48:51 +0530

    Refactor store.js: Improve code formatting and readability

--- LMS/FrontEnd/src/app/store.js
+++ LMS/FrontEnd/src/app/store.js
@@ -5,11 +5,14 @@ import { courseApi } from "@/features/api/courseApi";
 
 export const appStore = configureStore({
   reducer: rootReducer,
-  middleware: (defaultMiddleware) => defaultMiddleware().concat(authApi.middleware, courseApi.middleware),
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(authApi.middleware, courseApi.middleware),
 });
 
 const initializeApp = async () => {
-  await appStore.dispatch(authApi.endpoints.getUserProfile.initiate({}, { forceRefetch: true }));
-}
+  await appStore.dispatch(
+    authApi.endpoints.getUserProfile.initiate({}, { forceRefetch: true })
+  );
+};
 
 initializeApp();
