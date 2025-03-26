99a4746c8ded83301187de7ce8c8a0b7b151d050
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-03-26 23:22:07 +0530

    Update axios instance base URL to include API versioning for improved endpoint structure

--- AmiShop/frontend/src/api/axiosInstance.js
+++ AmiShop/frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const apiUrl = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
-  baseURL: apiUrl,
+  baseURL: `${apiUrl}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
