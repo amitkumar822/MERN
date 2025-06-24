88d88c4d61bb6cced0d0dd42c6c3dec69b2916cc
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-06-24 22:43:48 +0530

    Comment out Google login middleware and routes for future integration

--- AmiShop/backend/app.js
+++ AmiShop/backend/app.js
@@ -26,15 +26,15 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 // this middleware for google login
-app.use(
-  session({
-    secret: process.env.GOOGLE_CLIENT_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//   session({
+//     secret: process.env.GOOGLE_CLIENT_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // import router
 import userRouter from "./src/routes/user.routes.js";
@@ -42,7 +42,7 @@ import productRouter from "./src/routes/product.routes.js";
 import bannerSliderRouter from "./src/routes/bannerSlider.controller.routes.js";
 import desktopBannerSliderRouter from "./src/routes/desktopBannerSlider.routes.js";
 import orderRoutes from "./src/routes/order.routes.js";
-import googleRoutes from "./src/routes/google.routes.js";
+// import googleRoutes from "./src/routes/google.routes.js";
 import reviewRoutes from "./src/routes/review.routes.js";
 import topBrandRoutes from "./src/routes/topBrand.routes.js";
 
@@ -52,7 +52,7 @@ app.use("/api/v1/product", productRouter);
 app.use("/api/v1/banner-slider", bannerSliderRouter);
 app.use("/api/v1/desktop-banner-slider", desktopBannerSliderRouter);
 app.use("/api/v1/order", orderRoutes);
-app.use("/api/v1/auth", googleRoutes); // integrate with google login routes
+// app.use("/api/v1/auth", googleRoutes); // integrate with google login routes
 app.use("/api/v1/review", reviewRoutes);
 app.use("/api/v1/top-brand", topBrandRoutes);
 
