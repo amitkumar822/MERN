2b541c6064fac601ad60919e07344d6af61a6a92
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-06-25 13:47:57 +0530

    Update package-lock.json and clean up unused code in adminAuth and userAuth middlewares

--- AmiShop/backend/src/middlewares/adminAuth.js
+++ AmiShop/backend/src/middlewares/adminAuth.js
@@ -73,22 +73,3 @@ export const isAdminAuth = asyncHandler(async (req, res, next) => {
     }
   }
 });
-
-// import { asyncHandler } from "../../utils/asyncHandler.js";
-// import { ApiError } from "../../utils/ApiError.js";
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/user.model.js";
-
-// export const isAdminAuth = asyncHandler(async (req, res, next) => {
-//   const token = req.cookies?.jwt;
-//   if (!token) {
-//     throw new ApiError(401, "User Not Authenticated");
-//   }
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-//   const user = await User.findById(decoded.userId);
-//   if (user?.role !== "ADMIN") {
-//     throw new ApiError(403, "Unauthorized Access");
-//   }
-//   next();
-// });
--- AmiShop/backend/src/middlewares/userAuth.js
+++ AmiShop/backend/src/middlewares/userAuth.js
@@ -81,33 +81,3 @@ export const isAuthenticated = asyncHandler(async (req, res, next) => {
     }
   }
 });
-
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/user.model.js";
-// import { asyncHandler } from "../../utils/asyncHandler.js";
-// import { ApiError } from "../../utils/ApiError.js";
-
-// export const isAuthenticated = asyncHandler(async (req, res, next) => {
-//   const token = req.cookies?.jwt;
-
-//   if (!token) {
-//     throw new ApiError(401, "Please login...");
-//   }
-
-//   const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-//   const user = await User.findById(decodedToken.userId);
-
-//   if (!user) {
-//     throw new ApiError(404, "User Not Found");
-//   }
-
-//   const userDetails = {
-//     userId: user._id,
-//     email: user.email,
-//     role: user.role,
-//   }
-
-//   req.user = userDetails;
-//   next();
-// });
